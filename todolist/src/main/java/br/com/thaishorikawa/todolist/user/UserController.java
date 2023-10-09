63efc9dc6a138da5374e5f97313dc355cb2671c5
thaishorikawa <thaishorikawa@hotmail.com>
2023-10-09 22:26:52 +0100

    chore: add user controller

---
+++ todolist/src/main/java/br/com/thaishorikawa/todolist/user/UserController.java
@@ -0,0 +1,32 @@
+package br.com.thaishorikawa.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /*
+     * String (texto)
+     * Integer (int) números inteiros
+     * Double (double) números 0.0000
+     * Float (float) números 0.000
+     * char (A C)
+     * Date (data)
+     * void
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+     }
+}
