24e628284c074089f499653047f706c50fb11f4e
thaishorikawa <thaishorikawa@hotmail.com>
2023-10-11 18:35:21 +0100

    chore: add save user implementation

--- todolist/src/main/java/br/com/thaishorikawa/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/thaishorikawa/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.thaishorikawa.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /*
-     * String (texto)
-     * Integer (int) números inteiros
-     * Double (double) números 0.0000
-     * Float (float) números 0.000
-     * char (A C)
-     * Date (data)
-     * void
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
      }
 }
